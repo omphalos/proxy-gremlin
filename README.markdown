7ab47dee6d454a20b27485cf47292ea6ab2994b0
omphalos <omph.alos@yahoo.com>
2013-03-09 22:28:04 -0600

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,42 @@
+Proxy Gremlin
+=============
+
+Proxy Gremlin is a tiny middleware library that helps you to intercept and alter proxied responses.
+It is a middleware tool intended for use with [node-http-proxy](https://github.com/nodejitsu/node-http-proxy).
+
+Example
+=======
+
+    var http = require('http')
+      , proxy = require('http-proxy')
+      , gremlin = require('proxy-gremlin')
+
+    http.createServer(function(req, res) { // start an http server
+
+      // tell proxy-gremlin to buffer the response
+      var buffer = new gremlin.Buffer(res)
+
+      // set up a listener for when the proxied response is ready
+      res.on('end', function() {
+
+        console.log(buffer.headers)                  // log the headers
+        console.log(buffer.encoding)                 // log the specified encoding
+        console.log(buffer.getData())                // log buffered data
+
+        buffer.headers['Content-Type'] = 'text/html' // change a header
+        buffer.setData('Hello world')                // change the response's data
+        buffer.send()                                // send the response
+      })
+
+      // Proxy the request
+      proxy.proxyRequest(req, res, {
+        host: proxiedHost,
+        port: proxiedPort
+      })
+
+    }).listen(8080)
+
+License
+=======
+
+MIT
---
+++ index.js
@@ -0,0 +1,71 @@
+var util = require('util')
+
+function Buffer(res) {
+
+  var self = this
+
+  self.res = res
+
+  // Incoming data is stored in headers and _chunks.
+  self.headers = {}
+  self._chunks = []
+
+  // Monkey patch the response.
+  self._end = res.end
+  self._write = res.write
+  self._writeHead = res.writeHead
+
+  // Helper function for saving written chunks.
+  function write(chunk, encoding) {
+
+    if(chunk) self._chunks.push(chunk)
+    if(encoding) self.encoding = encoding
+  }
+
+  // Intercept written headers and save them for later.
+  res.writeHead = function(statusCode, headers) {
+
+    self.headers = headers
+    self._statusCode = statusCode
+
+    res.emit('writeHead', headers, statusCode)
+  }
+
+  // Intercept written chunks and save them for later.
+  res.write = function(chunk, encoding) {
+
+    write(chunk, encoding)
+
+    res.emit('write', chunk, encoding)
+  }
+
+  // Intercept response end.  Normally this will send the response to the client.
+  res.end = function(chunk, encoding) {
+
+    write(chunk, encoding)
+
+    res.emit('end', chunk, encoding)
+  }
+}
+
+// If setData has been called, return that, otherwise return intercepted data.
+Buffer.prototype.getData = function getData() {
+  return this._data || this._chunks.join('')
+}
+
+// Overwrite the response data.
+Buffer.prototype.setData = function setData(data) {
+
+  this.headers['content-length'] = data.length
+  this._data = data
+}
+
+// Since Buffer intercepts res.end,
+// send provides a way for the user to send the response to the client.
+Buffer.prototype.send = function send() {
+
+  this._writeHead.call(this.res, this._statusCode, this.headers)
+  this._end.call(this.res, this.getData(), this.encoding)
+}
+
+exports.Buffer = Buffer
